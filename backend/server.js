67a561cf7b1f056c07780f4ff15d772b3115a471
Ty Hastings <hastingsty44@gmail.com>
2024-10-17 12:43:41 -0500

    Firestore setup

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,15 @@ const express = require('express')
 const expressGraphQL = require('express-graphql').graphqlHTTP
 const cors = require('cors')
 const schema = require('./schema/schema')
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../config/graphql-8e395-firebase-adminsdk-oxdmz-54fedf0083.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const database = admin.firestore();
 
 const app = express()
 app.use(cors())
